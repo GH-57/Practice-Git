d2f082e191f18b2f31afc9c416d710a0ea6e0f6a
GH-57 <plue05007@gmail.com>
2025-04-28 22:40:19 +0900

    연습장

---
+++ pr.py
@@ -0,0 +1,33 @@
+
+
+# # y, m, d = input().split('.')gh
+
+# # print(d,m,y, sep="-")
+
+
+# sep 는 ~으로 구분한다
+
+# a, b = input().split("-")
+# print(a, b, sep = "")
+
+
+# s = input()
+# print(s[0:2], s[2:4], s[4:])
+
+# a,b,c = input().split(':')
+# print(b)
+
+# a, b = input().split("\t")
+# c = float(a+b)
+# print(c)
+
+# a = float(input())
+# b = float(input())
+# print(a+b)
+
+a = int(input())
+b = int(input())
+a < b = True
+a >= b = False
+
+print(bool(a,b))
