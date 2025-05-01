adfdd36d44b3975e19b2434bacc9413ba81e66e3
GH-57 <plue05007@gmail.com>
2025-05-01 17:09:49 +0900

    과제

--- 과제.py
+++ 과제.py
@@ -84,6 +84,86 @@ b = len(a)
 print(f"문자열 길이: {b}\n첫 글자: {a[0]}\n마지막 글자: {a[-1]}")
 '''
 
-# 문제 13(하드)
+########### 문제 13(하드)
+'''
+name = input().split()
+a = str(n")
+'''
+############
+# 문제 14
+'''
+a = float(input())
+print(f"{a:.2f}")
+'''
+
+# 문제 15
+'''
+a = int(input())
+
+if a >= 65:
+    print("노년")
+elif a >= 35:
+    print("중장년")
+elif a >= 19:
+    print("청년")
+else:
+    print("미성년자")
+'''
+
+####################### 문제 16(하드)
+'''
 a = input().split()
-b = a
+b = list(a)
+
+# if ' ' in b and int(b) in b:
+print(f"공백 수: {len(' ')}\n숫자 수: {b.count(int)}\n문자 수: {len(b)}")
+'''
+############################
+# 문제 17(질문)
+'''
+a = int(input())
+b = int(input())
+c = input()
+d = input()
+e = input()
+f = input()
+
+if a == 0:
+    print(bool(a))
+elif a !=0:
+    print(bool(a))
+if b == 0:
+    print(bool(b))
+elif b !=0:
+    print(bool(b))
+if c == '""':
+    print(bool(c))
+elif c!= '""':
+    print(bool(c)) ## 어디서 안되는 건지..
+if d == "":
+    print(bool(d))
+elif d!= "":
+    print(bool(d))
+if e == "":
+    print(bool(e))
+elif e!= "":
+    print(bool(e))
+if f == "":
+    print(bool(f))
+elif f!= "":
+    print(bool(f))
+'''
+# 문제 18
+'''
+a = int(input())
+
+if a%2 == 0: #짝수이면
+    print(f"{a}은(는) 짝수입니다.")
+else:
+    print(f"{a}은(는) 홀수입니다.")
+'''    
+# 문제 19
+s = input().split
+a = list(s)
+
+','.join(a)
