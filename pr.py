66449e856447fca186d231b93c94b9317747b9fd
GH-57 <plue05007@gmail.com>
2025-05-14 17:26:26 +0900

    백준 연습장

--- pr.py
+++ pr.py
@@ -80,3 +80,28 @@
 
 # print(sum)
 
+
+# X = int(input())
+# N = int(input())
+# sum = 0
+
+# for i in range(N):
+#     a, b = map(int,input().split())
+#     sum += a * b
+# if X == sum:
+#     print('Yes')
+
+# else:
+#     print("No")
+
+
+
+# n = int(input())
+
+# for i in range(1, (n+1)):
+#     print('*' * i)
+
+# n = int(input())
+
+# for i in range(1, n+1):
+#     print(" "*(n-i) + "*"*i )
